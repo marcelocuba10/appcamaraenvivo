05a290eb136fe6a8b45eb210815823a59eaf4719
Marcelo <marcelo.cuba.10@hotmail.com>
2024-05-16 15:56:40 -0400

    FIX name app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'app.quetalelpuente.ggcode',
-  appName: '¿Qué tal el puente?',
+  appName: 'Que tal el puente',
   webDir: 'www'
 };
 
