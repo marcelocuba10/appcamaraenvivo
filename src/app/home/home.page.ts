4896d1727dfb4323dbba869ddc1a170938a0053c
Marcelo <marcelo.cuba.10@hotmail.com>
2024-05-17 14:52:33 -0400

    FIX appereance page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,7 +10,14 @@ export class HomePage implements AfterViewInit {
   @ViewChild('videoPlayer1') videoPlayer1!: ElementRef<HTMLVideoElement>;
   @ViewChild('videoPlayer2') videoPlayer2!: ElementRef<HTMLVideoElement>;
 
-  constructor() {}
+  views1: number = 0;
+  views2: number = 0;
+  views3: number = 0;
+  views4: number = 0;
+  views5: number = 0;
+  views6: number = 0;
+
+  constructor() { }
 
   ngAfterViewInit() {
     const video1 = this.videoPlayer1.nativeElement;
@@ -23,26 +30,36 @@ export class HomePage implements AfterViewInit {
       hls1.loadSource(videoSrc1);
       hls1.attachMedia(video1);
       hls1.on(Hls.Events.MANIFEST_PARSED, () => {
-        video1.play(); // Reproducir automáticamente al cargar
+        // No reproducir automáticamente al iniciar
       });
 
       const hls2 = new Hls();
       hls2.loadSource(videoSrc2);
       hls2.attachMedia(video2);
       hls2.on(Hls.Events.MANIFEST_PARSED, () => {
-        video2.play(); // Reproducir automáticamente al cargar
+        // No reproducir automáticamente al iniciar
       });
     } else if (video1.canPlayType('application/vnd.apple.mpegurl') && video2.canPlayType('application/vnd.apple.mpegurl')) {
       video1.src = videoSrc1;
       video1.addEventListener('loadedmetadata', () => {
-        video1.play(); // Reproducir automáticamente al cargar
+        // No reproducir automáticamente al iniciar
       });
 
       video2.src = videoSrc2;
       video2.addEventListener('loadedmetadata', () => {
-        video2.play(); // Reproducir automáticamente al cargar
+        // No reproducir automáticamente al iniciar
       });
     }
+
+    // Simulación de visualizaciones
+    this.views1 = this.getRandomNumber();
+    this.views2 = this.getRandomNumber();
+
+    // Inicialización de visualizaciones para los iframes
+    this.views3 = this.getRandomNumber();
+    this.views4 = this.getRandomNumber();
+    this.views5 = this.getRandomNumber();
+    this.views6 = this.getRandomNumber();
   }
 
   // Método para alternar la reproducción del video
@@ -53,5 +70,9 @@ export class HomePage implements AfterViewInit {
     } else {
       video.pause();
     }
+  }
+
+  getRandomNumber(): number {
+    return Math.floor(Math.random() * 10000) + 1; // Número aleatorio entre 1 y 10000
   }
 }
