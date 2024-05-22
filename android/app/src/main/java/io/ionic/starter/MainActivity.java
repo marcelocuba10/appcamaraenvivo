7c4fa6bb29b56d2fd64723ca3f283701f510ca26
marcelocuba10 <marcelo.cuba.10@hotmail.com>
2024-05-22 18:57:48 -0400

    ADD ion-refresher

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,5 +1,14 @@
 package app.quetalelpuente.ggcode;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.google.android.gms.ads.MobileAds;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Inicializar AdMob
+        MobileAds.initialize(this, initializationStatus -> {});
+    }
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,9 @@
 import { Component, AfterViewInit, ViewChildren, ElementRef, QueryList, ChangeDetectorRef } from '@angular/core';
 import { WebcamService } from '../services/webcam.service';
 import Hls from 'hls.js';
-import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { DomSanitizer } from '@angular/platform-browser';
+import { AdMob, BannerAdOptions, BannerAdPosition, BannerAdSize } from '@capacitor-community/admob';
+import { IonRefresher } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -18,9 +20,37 @@ export class HomePage implements AfterViewInit {
     private webcamService: WebcamService,
     private sanitizer: DomSanitizer,
     private cdr: ChangeDetectorRef
-  ) {}
+  ) { }
 
-  ngAfterViewInit() {
+  async ngAfterViewInit() {
+    await AdMob.initialize();
+    try {
+      await AdMob.initialize();
+      this.showBanner();
+    } catch (error) {
+      console.error('AdMob initialization error:', error);
+    }
+
+    this.loadWebcams();
+  }
+
+  async showBanner() {
+    const options: BannerAdOptions = {
+      adId: 'ca-app-pub-9253092378386410/2148604195',
+      adSize: BannerAdSize.ADAPTIVE_BANNER,
+      position: BannerAdPosition.BOTTOM_CENTER,
+      margin: 0,
+      isTesting: false // Configura esto en false para producción
+    };
+
+    try {
+      await AdMob.showBanner(options);
+    } catch (error) {
+      console.error('Error showing banner ad:', error);
+    }
+  }
+
+  loadWebcams() {
     this.webcamService.getWebcams().subscribe((data) => {
       this.webcams = data.map((webcam: any) => {
         if (webcam.type === 'Iframe') {
@@ -72,5 +102,10 @@ export class HomePage implements AfterViewInit {
 
   getRandomNumber(): number {
     return Math.floor(Math.random() * 10000) + 1; // Número aleatorio entre 1 y 10000
+  }
+
+  doRefresh(event: any) {
+    this.loadWebcams();
+    event.target.complete(); // Finalizar la acción de refresco
   }
 }
